72653e03820e53915004fcb6736e6d63f25baffe
Hsiaoming Yang <me@lepture.com>
2014-09-01 20:18:19 +0800

    Play with QuickTime Player

--- removie/main.go
+++ removie/main.go
@@ -4,31 +4,44 @@ import (
 	"os"
 	"fmt"
 	"strings"
+	"os/exec"
 )
 import "github.com/lepture/removie"
 
 type service interface {
-	M3U8(url string) removie.PlayList
+	M3U8(uri string) removie.PlayList
 }
 
-func findService(url string) service {
-	if strings.Contains(url, "tv.sohu.com/") {
+func findService(uri string) service {
+	if strings.Contains(uri, "tv.sohu.com/") {
 		return &removie.Sohu{}
-	} else if strings.Contains(url, "youku.com/") {
+	} else if strings.Contains(uri, "youku.com/") {
 		return &removie.Youku{}
-	} else if strings.Contains(url, "tudou.com/") {
+	} else if strings.Contains(uri, "tudou.com/") {
 		return &removie.Tudou{}
 	} else {
 		return nil
 	}
 }
 
+
+func macPlay(uri string) {
+	fmt.Println("Playing: %s", uri)
+	cmd := exec.Command("open", "-a", "QuickTime Player", uri)
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(out)
+}
+
+
 func main() {
 	var s service;
-	url := os.Args[1]
-	s = findService(url)
+	uri := os.Args[1]
+	s = findService(uri)
 	if s != nil {
-		data := s.M3U8(url)
-		fmt.Println(data.Original)
+		data := s.M3U8(uri)
+		macPlay(data.Original)
 	}
 }
