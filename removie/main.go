495cfc73896ae1539e11466816521c5a69cc6d8c
Hsiaoming Yang <me@lepture.com>
2014-09-07 20:47:56 +0800

    Changes for binary

--- removie/main.go
+++ removie/main.go
@@ -26,13 +26,11 @@ func findService(uri string) service {
 
 
 func macPlay(uri string) {
-	fmt.Println("Playing: %s", uri)
 	cmd := exec.Command("open", "-a", "QuickTime Player", uri)
-	out, err := cmd.Output()
+	_, err := cmd.Output()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(out)
 }
 
 
@@ -40,8 +38,13 @@ func main() {
 	var s service;
 	uri := os.Args[1]
 	s = findService(uri)
-	if s != nil {
-		data := s.M3U8(uri)
-		macPlay(data.Original)
+	if s == nil {
+		return
+	}
+	data := s.M3U8(uri)
+	m3uURL := data.High
+	fmt.Println("Playing: ", m3uURL)
+	if m3uURL != "" {
+		macPlay(m3uURL)
 	}
 }
